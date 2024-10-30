58592f968bc47e5ec51b68be662d354841a94c35
biplavpoudel <biplavpoudel764@gmail.com>
2024-10-30 22:13:39 +0545

    added new controller action thay creates a platform model from dto and returns the model as a dto using automapper

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
 using PlatformService.Dtos;
+using PlatformService.Models;
 
 namespace PlatformService.Controllers;
 
@@ -24,5 +25,32 @@ public class PlatformsController : ControllerBase
 
         var platformItems = _repository.GetAllPlatforms();
         return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
+    }
+
+    [HttpGet("{id}", Name = "GetPlatformById")]
+    public ActionResult<PlatformReadDto> GetPlatformById(int id)
+    {
+        var platformItem = _repository.GetPlatformById(id);
+
+        if (platformItem == null)
+        {
+            return NotFound();
+        }
+        else
+        {
+            return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+        }
+    }
+
+    [HttpPost]
+    public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+    {
+        var platformModel = _mapper.Map<Platform>(platformCreateDto);
+        _repository.CreatePlatform(platformModel);
+        _repository.SaveChanges();
+        
+        var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+        return CreatedAtRoute(nameof(GetPlatformById), new {id = platformReadDto.Id}, platformReadDto);
     }
 }
