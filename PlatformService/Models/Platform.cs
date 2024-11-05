a8d831e82fe2ff4044da328adb2a61096900fab2
biplavpoudel <biplavpoudel764@gmail.com>
2024-11-05 09:38:22 +0545

    tiny change

--- PlatformService/Models/Platform.cs
+++ PlatformService/Models/Platform.cs
@@ -10,8 +10,10 @@ public class Platform
 
     [Required]
     public string Name { get; set; }
+
     [Required]
     public string Publisher { get; set; }
+
     [Required]
     public string Cost { get; set; }
 }
