761955452033010c6ca27fadd886f98fe7f7945f
biplavpoudel <biplavpoudel764@gmail.com>
2024-11-10 04:35:50 +0000

    added AddControllers() services and app.MapController() in Program.cs

--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -9,6 +9,7 @@ public class Program
 
         // Add services to the container.
         builder.Services.AddAuthorization();
+        builder.Services.AddControllers();
 
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
@@ -23,9 +24,10 @@ public class Program
             app.UseSwaggerUI();
         }
 
-        app.UseHttpsRedirection();
+        // app.UseHttpsRedirection();
 
         app.UseAuthorization();
+        app.MapControllers();
 
         app.Run();
     }
