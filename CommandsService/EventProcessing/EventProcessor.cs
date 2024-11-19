2b6b663f7a8d0c7c1147f33bb862b5af069b86e7
biplavpoudel <biplavpoudel764@gmail.com>
2024-11-19 01:47:10 +0000

    added event processor interface

---
+++ CommandsService/EventProcessing/EventProcessor.cs
@@ -0,0 +1,15 @@
+namespace CommandsService.EventProcessing;
+
+public class EventProcessor : IEventProcessor
+{
+    public void ProcessEvent(string message)
+    {
+        throw new NotImplementedException();
+    }
+
+    enum EventType
+    {
+        PlatformPublished,
+        Undetermined
+    }
+}
