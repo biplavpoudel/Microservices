bb26e9b470d3db1bec670426adef4814d6be8af6
biplavpoudel <biplavpoudel764@gmail.com>
2024-11-19 12:52:36 +0000

    completed subscriber class

--- CommandsService/AsyncDataServices/MessageBusSubscriber.cs
+++ CommandsService/AsyncDataServices/MessageBusSubscriber.cs
@@ -1,5 +1,7 @@
+using System.Text;
 using CommandsService.EventProcessing;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace CommandsService.AsyncDataServices;
 
@@ -41,7 +43,23 @@ public class MessageBusSubscriber : BackgroundService
 
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        throw new NotImplementedException();
+        stoppingToken.ThrowIfCancellationRequested();
+
+        var consumer = new EventingBasicConsumer(_channel);
+
+        consumer.Received += (ModuleHandle, args) =>
+        {
+            Console.WriteLine("--> Event Received!");
+
+            var body = args.Body;
+            var notificationMessage = Encoding.UTF8.GetString(body.ToArray());
+
+            _eventProcessor.ProcessEvent(notificationMessage);
+        };
+
+        _channel.BasicConsume(queue: _queueName, autoAck: true, consumer: consumer);
+
+        return Task.CompletedTask;
     }
 
     private void RabbitMQ_ConnectionShutdown(object sender, EventArgs e)
